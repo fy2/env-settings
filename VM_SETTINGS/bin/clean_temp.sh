a5bf7eacc05ef23d45a2c33a75c4a27560347895
Feyruz Yalcin <feyruz.yalcin@wcn.co.uk>
2016-10-20 14:37:54 +0100

    updates

--- VM_SETTINGS/bin/clean_temp.sh
+++ VM_SETTINGS/bin/clean_temp.sh
@@ -1,20 +1,18 @@
 confirm() {
   echo -n "Do you want to run: $*? [N/y] "
-  read -N 1 REPLY 
+  read -N 1 REPLY
   echo
 
-  if test "$REPLY" = "y" -o "$REPLY" = "Y"; then 
-    "$@" 
-  else 
-    echo "Cancelled by user" 
-  fi 
+  if test "$REPLY" = "y" -o "$REPLY" = "Y"; then
+    "$@"
+  else
+    echo "Cancelled by user"
+  fi
 }
 
 if [[ -n $(ls /tmp | grep -P "^(ttc|vx)" ) ]]; then
   echo 'sudo chown -R feyruz.feyruz /tmp/ttc* /tmp/vx*'
-  sudo chown -R feyruz.feyruz /tmp/ttc* /tmp/vx*
+  sudo chown -R feyruz.feyruz /tmp/ttc* /tmp/vx* /tmp/FEYRUZ*.log /tmp/sql.*txt
 fi
 
-confirm rm -Rf /tmp/ttc* /tmp/vx*
-
-
+confirm rm -Rf /tmp/ttc* /tmp/vx* /tmp/FEYRUZ*.log /tmp/sql.*txt
